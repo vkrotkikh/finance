aeb4e82d38ba10d79b0e2a02cb74b35264523ab4
vkrotkikh <krotkikhvadim@gmail.com>
2023-07-17 17:10:11 -0400

    add port

--- back/index.ts
+++ back/index.ts
@@ -50,7 +50,7 @@ startStandaloneServer(server, {
 })
 */
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
